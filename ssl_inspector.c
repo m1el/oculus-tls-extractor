474a15b2e0ee11cf43a61ad2eeee8beb399e7e78
Igor null <m1el.2027@gmail.com>
2020-04-06 18:01:22 +0300

    documented ssl_inspector module

--- ssl_inspector.c
+++ ssl_inspector.c
@@ -1,9 +1,21 @@
+/*
+ ssl_inspector module is responsible for extracting private keys
+ from ssl state.  It is a more robust approach to build a C module
+ than maintaining a representation of OpenSSL structs in Rust or
+ manually chase pointers in Rust.
+ */
 #include <stdio.h>
 #include <stdint.h>
 #include "ssl_locl.h"
 
 typedef unsigned char u8;
 
+/*
+ private_keys struct is used to communicate with Rust code.
+ The accomanying struct in Rust injectee is PkData.
+ It bears information about two slices in memory --
+ for client random and master key.
+ */
 typedef struct {
     u8* client_random;
     size_t client_random_size;
@@ -11,6 +23,14 @@ typedef struct {
     size_t master_key_size;
 } private_keys;
 
+/*
+ ssl_read_pk_data: Extract private keys from openssl struct.
+ this function populates output with pointers to-- and lengths
+ of private key data.
+ Arguments:
+   ssl: pointer to ssl struct
+   output: pointer to output struct
+ */
 extern
 void ssl_read_pk_data(struct ssl_st* ssl, private_keys* output) {
     if (ssl == NULL || output == NULL) {
@@ -27,8 +47,16 @@ void ssl_read_pk_data(struct ssl_st* ssl, private_keys* output) {
     }
 }
 
+/* typedef of function pointer for easier use later */
 typedef int (*ssl_connect_fn) (SSL *s);
 
+/*
+ ssl_connect_fn: extract function pointer from openssl state.
+ Arguments:
+   ssl: pointer to ssl struct
+ Return value:
+   pointer to ssl_connect function of ssl_method struct
+ */
 extern
 ssl_connect_fn ssl_get_ssl_connect(struct ssl_st* ssl) {
     if (ssl != NULL && ssl->method != NULL) {
